9104c038218e5b6effc548cd9bc5234c439a7166
Jose Kober <jkober@santafe.gov.ar>
2021-08-30 08:23:03 -0300

    group by

--- Resources/public/js/design/app/view/design/SetGroupBy.js
+++ Resources/public/js/design/app/view/design/SetGroupBy.js
@@ -30,7 +30,25 @@ Ext.define("AppDesign.view.design.SetGroupBy",{
 		var encuentra	= false
 		var field;
 		var field2;
-		for (var i = 0 ;i<kc.pdfStrucRefActiva.reportExtras.field.length;i++) {			
+		let field_unidos = kc.estructuro_field_json(kc.pdfStrucRefActiva.reportExtras.field,kc.pdfStrucRefActiva.field_json);
+		for (var i = 0 ;i<field_unidos.length;i++) {
+			field = field_unidos[i]
+			encuentra=false
+			for (var ii = 0 ; ii < this.group.GroupBy.length;ii++) {
+				field2 = this.group.GroupBy[ii]
+				if (field.name == field2) {
+					encuentra=true
+					break;
+				}
+			}
+			if (encuentra) {
+				lista2.push(field)
+			}else{
+				lista1.push(field)
+			}
+		}
+
+/*		for (var i = 0 ;i<kc.pdfStrucRefActiva.reportExtras.field.length;i++) {
 			field = kc.pdfStrucRefActiva.reportExtras.field[i]
 			encuentra=false
 			for (var ii = 0 ; ii < this.group.GroupBy.length;ii++) {
@@ -44,9 +62,46 @@ Ext.define("AppDesign.view.design.SetGroupBy",{
 				lista2.push(field)	
 			}else{
 				lista1.push(field)	
-
 			}
 		}
+		//--------------------------------------------------------------------------------------------------------------
+		if (typeof kc.pdfStrucRefActiva.field_json == "string") {
+			try {
+				if ( kc.pdfStrucRefActiva.field_json.trim()!="") {
+					let xx_f = JSON.parse("[" + kc.pdfStrucRefActiva.field_json + "]");
+					let o;
+					for (let i=0 ;i<xx_f.length ;i++){
+						o=xx_f[i];
+						try {
+							field 			= {};
+							field.name 		= o[0];
+							field.nameDisp 	= o[1];
+							field.extra = {"nameDisp": o[1], "name": o[0],"ff": o[3],"maxLength":o[2]};
+						}catch (e) {
+							alert("errores al procesar el campo :" + i )
+						}
+						encuentra=false
+						for (var ii = 0 ; ii < this.group.GroupBy.length;ii++) {
+							field2 = this.group.GroupBy[ii]
+							if (field.name == field2) {
+								encuentra=true
+								break;
+							}
+						}
+						if (encuentra) {
+							lista2.push(field)
+						}else{
+							lista1.push(field)
+						}
+
+					}
+				}
+			}catch (e) {
+				alert("errores al procesar los field extras")
+			}
+		}
+*/
+		//--------------------------------------------------------------------------------------------------------------
 		this.stFrom.loadData(lista1)
 		this.stTo.loadData(lista2)
 		me.items = [
