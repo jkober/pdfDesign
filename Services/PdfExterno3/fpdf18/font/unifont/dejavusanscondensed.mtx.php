4409a763dc91e176b100827d3af523ea733d2327
Jose Kober <jkober@santafe.gov.ar>
2024-07-10 10:28:28 -0300

    INIT

--- Services/PdfExterno3/fpdf18/font/unifont/dejavusanscondensed.mtx.php
+++ Services/PdfExterno3/fpdf18/font/unifont/dejavusanscondensed.mtx.php
@@ -13,7 +13,7 @@ $desc=array (
 );
 $up=-63;
 $ut=44;
-$ttffile='/var/www/html/rcadmin/vendor/jkober/design/Services/PdfExterno3/fpdf18/font/unifont/DejaVuSansCondensed.ttf';
+$ttffile=__DIR__.'/DejaVuSansCondensed.ttf';
 $originalsize=680264;
 $fontkey='dejavu';
 ?>
